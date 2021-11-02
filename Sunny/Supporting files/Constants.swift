a57b7dca36f65320638b49c698c8d9b087683e41
Nsi4b <Nsi4b@yahoo.com>
2021-11-02 03:11:36 +0300

    first

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Philip on 23.07.2021.
+//
+
+import Foundation
+
+let apiKey = "2da497baf38401c199c44659eeec6de0"
---
+++ Sunny/View Controllers/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Sunny
+//
+//  Created by Philip on 23.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    
+    let networkWeatherManager = NetworkWeatherManager()
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        networkWeatherManager.fetchCurrentWeather(forCity: "London")
+    }
+}
+
+
